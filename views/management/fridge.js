d3d1cea49e1bc02a92dedb8a24d501b98a75b8b6
nayeon729 <kimnayeon4729@gmail.com>
2025-02-27 11:08:01 +0900

    생상변경, 냉장고 표

---
+++ views/management/fridge.js
@@ -0,0 +1,33 @@
+import { db } from '../../js/utils/helpers.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+ dbbring();
+});
+
+
+
+async function dbbring(){
+    const { data, error } = await db
+    .from('ledger')
+    .select('*')
+    .eq('category', 'food');
+    console.log(data);
+    if (error) {
+        console.error("데이터 가져오기 실패:", error);
+        return;
+    }
+
+    const table = document.getElementById('itemTable');
+    
+    data.forEach(item => {
+        let row = `
+            <tr>
+                <td>${item.description}</td>
+                <td>${item.user_id}</td>
+                <td>${item.group_id}</td>
+               
+            </tr>
+        `;
+        table.innerHTML += row;
+    });
+}
